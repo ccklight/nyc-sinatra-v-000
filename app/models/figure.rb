5e1815cd07117312cc91a3e504f8da5daf9b6ad2
Cheryl K <cherylkelson@icloud.com>
2019-04-16 15:40:37 +0000

    Automatically backed up by Learn

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,5 +1,13 @@
 class Figure < ActiveRecord::Base
+<<<<<<< HEAD
   has_many :landmarks
   has_many :figure_titles
   has_many :titles, through: :figure_titles
+=======
+  # add relationships here
+  has_many :landmarks
+  has_many :figure_titles
+  has_many :titles, through: : figure_titles  
+
+>>>>>>> 3ba9928932918b985a9efa94f74312f17f90f49e
 end
